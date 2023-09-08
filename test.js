cc467a3245d0ed025068deb6260a9f3eca274c90
Titus Wormer <tituswormer@gmail.com>
2023-09-08 15:25:31 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -1,48 +1,44 @@
-import assert from 'node:assert'
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {retext} from 'retext'
 import {normalize} from 'nlcst-normalize'
 import pluralize from 'pluralize'
 import {schema} from './lib/schema.js'
 import retextRedundantAcronyms from './index.js'
 
-const own = {}.hasOwnProperty
+test('retextRedundantAcronyms', async function (t) {
+  await t.test('should emit a message w/ metadata', async function () {
+    const file = await retext()
+      .use(retextRedundantAcronyms)
+      .process('Where can I find an ATM machine?')
 
-test('retext-redundant-acronyms', (t) => {
-  t.plan(2)
+    assert.deepEqual(
+      JSON.parse(JSON.stringify(file.messages)),
+      [
+        {
+          column: 21,
+          fatal: false,
+          message: 'Expected `ATM` instead of `ATM machine`',
+          line: 1,
+          name: '1:21-1:32',
+          place: {
+            start: {line: 1, column: 21, offset: 20},
+            end: {line: 1, column: 32, offset: 31}
+          },
+          reason: 'Expected `ATM` instead of `ATM machine`',
+          ruleId: 'atm',
+          source: 'retext-redundant-acronyms',
+          actual: 'ATM machine',
+          expected: ['ATM'],
+          url: 'https://github.com/retextjs/retext-redundant-acronyms#readme'
+        }
+      ],
+      'should warn about redundant acronyms'
+    )
+  })
 
-  retext()
-    .use(retextRedundantAcronyms)
-    .process('Where can I find an ATM machine?')
-    .then((file) => {
-      t.deepEqual(
-        JSON.parse(JSON.stringify(file.messages)),
-        [
-          {
-            column: 21,
-            fatal: false,
-            message: 'Expected `ATM` instead of `ATM machine`',
-            line: 1,
-            name: '1:21-1:32',
-            place: {
-              start: {line: 1, column: 21, offset: 20},
-              end: {line: 1, column: 32, offset: 31}
-            },
-            reason: 'Expected `ATM` instead of `ATM machine`',
-            ruleId: 'atm',
-            source: 'retext-redundant-acronyms',
-            actual: 'ATM machine',
-            expected: ['ATM'],
-            url: 'https://github.com/retextjs/retext-redundant-acronyms#readme'
-          }
-        ],
-        'should warn about redundant acronyms'
-      )
-    }, t.ifErr)
-
-  retext()
-    .use(retextRedundantAcronyms)
-    .process(
+  await t.test('should emit messages', async function () {
+    const file = await retext().use(retextRedundantAcronyms).process(
       [
         // Abbreviation in sentence.
         'Is there an ATM nearby?',
@@ -65,67 +61,53 @@ test('retext-redundant-acronyms', (t) => {
         'Whenever I read "CSS stylesheets" my brain automatically translates it…'
       ].join('\n')
     )
-    .then((file) => {
-      t.deepEqual(
-        file.messages.map(String),
-        [
-          '3:5-3:32: Expected `HIV` instead of `HIV immuno deficiency virus`',
-          '4:5-4:32: Expected `HIV` instead of `HIV immuno-deficiency virus`',
-          '5:1-5:16: Expected `GRE` instead of `GRE examination`',
-          '5:21-5:29: Expected `GRE` instead of `GRE exam`',
-          '9:11-9:23: Expected `ATMs` instead of `ATM machines`',
-          '11:18-11:33: Expected `CSSs` instead of `CSS stylesheets`'
-        ],
-        'should warn about redundant acronyms'
-      )
-    }, t.ifErr)
+
+    assert.deepEqual(file.messages.map(String), [
+      '3:5-3:32: Expected `HIV` instead of `HIV immuno deficiency virus`',
+      '4:5-4:32: Expected `HIV` instead of `HIV immuno-deficiency virus`',
+      '5:1-5:16: Expected `GRE` instead of `GRE examination`',
+      '5:21-5:29: Expected `GRE` instead of `GRE exam`',
+      '9:11-9:23: Expected `ATMs` instead of `ATM machines`',
+      '11:18-11:33: Expected `CSSs` instead of `CSS stylesheets`'
+    ])
+  })
 })
 
-test('schema', (t) => {
-  t.doesNotThrow(() => {
+test('schema', async function (t) {
+  await t.test('should include normal words', async function () {
     /** @type {string} */
     let key
 
     for (key in schema) {
-      if (own.call(schema, key)) {
+      if (Object.hasOwn(schema, key)) {
         const list = schema[key].flat()
-
         let index = -1
+
         while (++index < list.length) {
           const word = list[index]
-          assert.strictEqual(
-            normalize(word),
-            word,
-            '`' + word + '` should be normal'
-          )
+
+          assert.strictEqual(normalize(word), word)
         }
       }
     }
-  }, 'all words should be normalized')
+  })
 
-  t.doesNotThrow(() => {
+  await t.test('should include singulars', async function () {
     const ignore = new Set(['trans'])
     /** @type {string} */
     let key
 
     for (key in schema) {
-      if (own.call(schema, key)) {
+      if (Object.hasOwn(schema, key)) {
         const list = schema[key].flat()
-
         let index = -1
         while (++index < list.length) {
           const word = list[index]
 
-          if (!ignore.has(word)) {
-            assert.ok(
-              pluralize.isSingular(word),
-              '`' + word + '` should be singular'
-            )
-          }
+          if (ignore.has(word)) continue
+          assert.ok(pluralize.isSingular(word))
         }
       }
     }
-  }, 'all words should be singular')
-
-  t.end()
+  })
 })
